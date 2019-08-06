9e56c59ec162a4a214d0cca68d513dc7a7e9c0c5
Gary <gp@limestripes.us>
2019-08-06 13:29:30 -0600

    begin bootstrap layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MapContainerComponent } from './map-container/map-container.component';
+import { AccordionComponent } from './accordion/accordion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    MapContainerComponent,
+    AccordionComponent
   ],
   imports: [
     BrowserModule
